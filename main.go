ce07ec5f37d3d14e12e7a4481ea1f74a5b0f852a
Danpa_cho <danpa725@cau.ac.kr>
2022-11-22 15:45:08 +0900

    update: go의 함수를 학습해보았습니다

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
-// main.go는 컴파일 진입점이다.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	// 상수는 const [상수이름] [상수타입] = 상수
-	const name string = "danpacho"
+	var mult int = multiply(1, 2)
 
-	// 기본적으로 변수는 var [변수이름] [변수타입] = 변수 로 선언한다.
-	var varName string = "my-friend"
-	varName = "jkfds"
+	// 2개를 동시에 반환하는 lenAndUpper 함수
+	totalLength, upperName := lenAndUpper("puang")
+	// _ 를 활용하면 변수를 ignore 할 수 있다.
+	yeah, _ := lenAndUpper("ignored")
 
-	// 축약형 변수는 [변수이름] := 변수 와 같은 형태로 선언 가능하고, 오직 "함수 func" 내부에서만 사용할 수 있다.
-	// 또한 타입은 Go가 스스로 유추해주며 타입을 재지정할 수 없다.
-	convienientVarName := "esasy"
-	convienientVarName = "yeahs"
+	names := repeatNames("june", "me", "dan")
+	singleName := funcWithArguments("june", "done", "hande")
 
-	fmt.Println(name, varName, convienientVarName)
+	fmt.Println(mult, totalLength, upperName, names, singleName, yeah)
+}
+
+// 인자가 타입을 공유하는 경우 a int, b int를 a, b int로 축약가능하다.
+func multiply(a, b int) int {
+	return a * b
+}
+
+// go의 함수는 많은 것을 동시에 반환할 수 있다.
+// func [funcName(arg)] (return 타입1, return 타입2, return타입3, ...)
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToTitle(name)
+}
+
+// 자바스크립트 나머지 매게 변수 처럼 사용할 때, ...[타입]
+func funcWithArguments(arg ...string) string {
+	return strings.Join(arg, "")
+}
+
+// repeatNames names 나머지 매개 변수 ...string
+func repeatNames(names ...string) []string {
+	return names
 }
